5ea1f34e1b193f7ebabb9787d05ce2c7b3410edc
Martin Billinger <flkazemakase@gmail.com>
2021-01-09 20:15:34 +0100

    missing function: membero

--- 7-more-languages-in-7-weeks/6-kanren/day-1/src/main.rs
+++ 7-more-languages-in-7-weeks/6-kanren/day-1/src/main.rs
@@ -1,6 +1,7 @@
 use mini_kanren::*;
 
 fn main() {
-    dbg!(run!(*, q { eq(q, 1); }));
-    dbg!(run!(*, q { eq(q.clone(), 1); eq(q, 2); }));
+    dbg!(run!(*, q, eq(q, 1)));
+    dbg!(run!(*, q, eq(q.clone(), 1), eq(q, 2)));
+    dbg!(run!(*, q, membero(q, [1, 2, 3])));
 }
