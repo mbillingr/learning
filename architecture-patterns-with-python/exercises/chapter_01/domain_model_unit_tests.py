3962409208387222907c08df5a465db04b0ce074
anon <n/a>
2021-01-11 11:36:01 +0100

    finish chapter 1 domain model

--- architecture-patterns-with-python/exercises/chapter_01/domain_model_unit_tests.py
+++ architecture-patterns-with-python/exercises/chapter_01/domain_model_unit_tests.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch, line = make_batch_and_line('SMALL-TABLE', 20, 2)
 
@@ -52,12 +55,56 @@ def make_batch_and_line(sku, batch_qty, line_qty):
     return batch, different_sku_line
 
 
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch('in-stock-batch', "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch('shipment-batch', "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine('oref', "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+
+def test_prefers_earlier_batches():
+    earliest = Batch('speedy-batch', 'MINIMALIST-SPOON', 100, eta=today)
+    medium = Batch('normal-batch', 'MINIMALIST-SPOON', 100, eta=tomorrow)
+    latest = Batch('slow-batch', 'MINIMALIST-SPOON', 100, eta=later)
+    line = OrderLine('order1', 'MINIMALIST-SPOON', 10)
+
+    allocate(line, [medium, earliest, latest])
+
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100
+
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch('in-stock-batch', "FUTURISTIC-CLOCK", 100, eta=None)
+    shipment_batch = Batch('shipment-batch', "FUTURISTIC-CLOCK", 100, eta=tomorrow)
+    line = OrderLine('oref', 'FUTURISTIC-CLOCK', 10)
+    allocation = allocate(line, [in_stock_batch, shipment_batch])
+    assert allocation == in_stock_batch.reference
+
+
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch('batch1', 'SMALL-FORK', 10, eta=today)
+    allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+
+    with pytest.raises(OutOfStock, match='SMALL-FORK'):
+        allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+
+
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Sequence
+
+today = date(year=2020, month=1, day=1)
+tomorrow = date(year=2020, month=1, day=2)
+later = date(year=2020, month=2, day=1)
 
 
-class BatchAllocationError(Exception): pass
+class OutOfStock(Exception): pass
 
 
 @dataclass(frozen=True)
@@ -94,6 +141,20 @@ class Batch:
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
 
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
 
-def today():
-    pass
+        if other.eta is None:
+            return True
+
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: Sequence[Batch]) -> str:
+    try:
+        batch = min(b for b in batches if b.can_allocate(line))
+    except ValueError:
+        raise OutOfStock(f'Out of stock: {line.sku}')
+    batch.allocate(line)
+    return batch.reference
